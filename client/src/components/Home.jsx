412b0446b0cf0128935c9f74de569258e092b67b
imsaad <ishtiak.m.saad@gmail.com>
2025-10-15 00:57:45 +0600

    setup React Router v7 with react-router-dom , Redux Toolkit, tailwind

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='font-mono text-2xl'>
+            Discord Clone. Tailwind + Router Working correctly. Test Passed!
+        </div>
+    );
+};
+
+export default Home;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />,
+  </React.StrictMode>
+);
