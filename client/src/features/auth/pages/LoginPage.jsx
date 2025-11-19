7d1ee55f92efa50d6a4b63760fcc4d98a9d32617
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-11-19 22:11:50 +0600

    Create overall frontend app structure and architecture

---
+++ client/src/features/auth/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router'
+
+const LoginPage = () => {
+    return (
+        <>
+            <h1>This is login Page</h1>
+            <Link to={'/register'}>Register Now</Link>
+        </>
+    )
+}
+
+export default LoginPage
---
+++ client/src/features/auth/pages/RegisterPage.jsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+    return <h1>This is register page</h1>
+}
+
+export default RegisterPage
