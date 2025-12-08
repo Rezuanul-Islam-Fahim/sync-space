617cf64297f86c88822df51b16bf38e2faa69146
imsaad <ishtiak.m.saad@gmail.com>
2025-12-08 23:54:57 +0600

    unused imports removed

--- client/src/features/auth/components/FormCheckbox.jsx
+++ client/src/features/auth/components/FormCheckbox.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const FormCheckbox = ({ name, checked, onChange, children }) => {
   return (
--- client/src/features/auth/pages/RegisterPage.jsx
+++ client/src/features/auth/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import FormInput from "../components/FormInput";
 import FormCheckbox from "../components/FormCheckbox";
