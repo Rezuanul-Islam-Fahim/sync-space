ada79c6b0407b8fbe3bdd7b5bcf87d38ab04ddd4
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-25 17:34:54 +0600

    Add helmet middleware for enhanced security

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import router from './api/routes/index.js'
 import { errorHandler } from './middlewares/errorHandler.js'
+import helmet from 'helmet'
 
 const createApp = () => {
     const app = express()
 
+    app.use(helmet())
     app.use(express.json())
 
     app.use('/api', router)
