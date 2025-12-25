d16e1873f7d179f8fe2b4f60213729969da50c30
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-25 17:37:07 +0600

    Add CORS middleware for cross-origin resource sharing

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import router from './api/routes/index.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const createApp = () => {
     const app = express()
 
     app.use(helmet())
+    app.use(cors())
     app.use(express.json())
 
     app.use('/api', router)
