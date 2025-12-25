d4b60e98d6826c47d802ffe2880d1be85d49631b
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-25 19:34:05 +0600

    Add morgan middleware for HTTP request logging

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,13 @@ import router from './api/routes/index.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import helmet from 'helmet'
 import cors from 'cors'
+import morgan from 'morgan'
+import { isDev } from './config/index.js'
 
 const createApp = () => {
     const app = express()
 
+    app.use(morgan(isDev() ? 'dev' : 'combined'))
     app.use(helmet())
     app.use(cors())
     app.use(express.json())
