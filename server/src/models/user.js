eb92f700db6fbca04407de4e70b67fb1107e651c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-12-26 17:20:36 +0600

    Enhance user schema by adding additional fields for avatar, bio, banner, bannerColor, dateOfBirth, isVerified, status, and lastOnline

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -7,9 +7,48 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true
         },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
         password: {
             type: String,
             required: true
+        },
+        avatar: {
+            type: String,
+            default: null
+        },
+        bio: {
+            type: String,
+            maxLength: 190,
+            default: null
+        },
+        banner: {
+            type: String,
+            default: null,
+        },
+        bannerColor: {
+            type: String,
+            default: null,
+        },
+        dateOfBirth: {
+            type: Date,
+            required: true
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        status: {
+            type: String,
+            enum: ['online', 'offline', 'idle', 'dnd'],
+            default: 'offline'
+        },
+        lastOnline: {
+            type: Date,
+            default: Date.now
         }
     },
     { timestamps: true }
