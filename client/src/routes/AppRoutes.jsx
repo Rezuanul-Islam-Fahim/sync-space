811078e7947fba921cfc0bee6aac29402084f2ab
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-01-08 17:30:03 +0600

    Add OnboardingPage component and update routing; include react-icons dependency

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router'
 import { LoginPage, RegisterPage } from '../features/auth'
+import { OnboardingPage } from '../features/onboarding'
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route index element={<LoginPage />} />
+            <Route index element={<OnboardingPage />} />
+            <Route path='/login' element={<LoginPage />} />
             <Route path='/register' element={<RegisterPage />} />
         </Routes>
     )
