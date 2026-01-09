9d92fc6ca3d39ffccdc793c7d31b41cd7a808f08
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-01-09 23:31:20 +0600

    Add clsx and tailwind-merge for class name management; refactor CommonButton to utilize cn utility for improved styling; remove unused sample.js file.

--- client/src/shared/components/CommonButton.jsx
+++ client/src/shared/components/CommonButton.jsx
@@ -1,21 +1,20 @@
 import { Link } from 'react-router';
+import cn from '@/shared/utils/cn';
 
-const CommonButton = ({ children, navigate, secondary = false }) => {
-  let className =
+const CommonButton = ({ children, navigate, secondary = false, className }) => {
+  const baseClassNames =
     'w-full text-white font-medium py-2.5 px-6 rounded-lg transition-colors duration-200 cursor-pointer';
-
-  if (!secondary) {
-    className += ' bg-discord-primary hover:bg-discord-primary-hover';
-  } else {
-    className += ' bg-discord-tertiary hover:bg-discord-tertiary-hover';
-  }
+  const secondaryClassNames = !secondary
+    ? 'bg-discord-primary hover:bg-discord-primary-hover'
+    : 'bg-discord-tertiary hover:bg-discord-tertiary-hover';
+  const combinedClassNames = cn(baseClassNames, secondaryClassNames, className);
 
   return navigate !== undefined ? (
-    <Link to={navigate} className={className}>
+    <Link to={navigate} className={combinedClassNames}>
       {children}
     </Link>
   ) : (
-    <button className={className} type="submit">
+    <button className={combinedClassNames} type="submit">
       {children}
     </button>
   );
