54c921912d54202de24f4940f8c7cfb0e0961788
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-05-27 19:28:05 +0600

    Refactor registration schema for improved validation messages and structure

--- client/src/features/auth/schemas/registerSchema.js
+++ client/src/features/auth/schemas/registerSchema.js
@@ -1,20 +1,28 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const registerSchema = z.object({
-  email: z
-    .string()
-    .nonempty({ message: 'This field is required' })
-    .email({ message: 'Invalid email address' }),
-  displayName: z.string(),
-  username: z.string().nonempty({ message: 'This field is required' }),
-  password: z
-    .string()
-    .nonempty({ message: 'This field is required' })
-    .min(6, { message: 'Must be at least 6 characters long' }),
-  dateOfBirth: z.string().nonempty({ message: 'This field is required' }),
-  agreeToTerms: z.literal(true, {
-    error: 'You must agree to the terms and conditions',
-  }),
-});
+  email: z.string()
+    .nonempty('Email is required')
+    .pipe(z.email('Please enter a valid email')),
+
+  displayName: z.string().optional(),
+
+  username: z.string()
+    .nonempty('Username is required')
+    .min(3, { error: 'Username must be at least 3 characters long' })
+    .max(30, { error: 'Username should be maximum of 30 characters' }),
+
+  password: z.string()
+    .nonempty('Password is required')
+    .min(6, { error: 'Password must be at least 6 characters' }),
+
+  dateOfBirth: z.string()
+    .nonempty('Date of Birth is required')
+    .pipe(z.coerce.date({ error: 'Please enter a valid date' })),
+
+  agreeToTerms: z.literal(
+    true, { error: 'You must agree to the terms' }
+  )
+})
 
 export default registerSchema;
