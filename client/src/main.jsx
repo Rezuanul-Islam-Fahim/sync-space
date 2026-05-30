2520f6f76bb422bfb72065872656349d9b8b12fa
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-05-30 15:29:21 +0600

    Add Redux store setup and integrate with Provider in main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './styles/index.css';
-import ReactDOM from 'react-dom/client';
-import App from './app';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import './styles/index.css'
+import App, { store } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
