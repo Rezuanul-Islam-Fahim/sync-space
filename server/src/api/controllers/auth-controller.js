0bada30ba649d7f198a08f90dfbe03c4c92740a0
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-05-31 12:52:36 +0600

    Refactor registration logic to use DTOs for request and response handling

--- server/src/api/controllers/auth-controller.js
+++ server/src/api/controllers/auth-controller.js
@@ -1,12 +1,16 @@
 import * as authService from '../../services/auth-service.js'
+import { RegisterRequestDto, RegisterResponseDto } from '../dtos/auth.dto.js'
 
 export const register = async (req, res, next) => {
     try {
-        const userData = await authService.registerUser(req.body)
+        const requestDto = RegisterRequestDto.from(req.body)
+        const userData = await authService.registerUser(requestDto)
+        const responseDto = RegisterResponseDto.from(userData)
+        console.log(responseDto)
 
         res.status(201).json({
             success: true,
-            data: userData,
+            data: responseDto,
             message: 'New user created successfully'
         })
     }
---
+++ server/src/api/dtos/auth.dto.js
@@ -0,0 +1,46 @@
+export class RegisterRequestDto {
+    constructor(data) {
+        this.email = data.email
+        this.username = data.username
+        this.password = data.password
+        this.displayName = data.displayName
+        this.agreeToTerms = data.agreeToTerms ?? false
+        this.avatar = data.avatar
+        this.bio = data.bio
+        this.banner = data.banner
+        this.bannerColor = data.bannerColor ?? '5865F2'
+        this.dateOfBirth = data.dateOfBirth
+        this.isVerified = data.isVerified
+        this.status = data.status
+        this.lastOnline = data.lastOnline
+    }
+
+    static from(body) {
+        return new RegisterRequestDto(body)
+    }
+}
+
+export class RegisterResponseDto {
+    constructor(user) {
+        this.id = user._id.toString()
+        this.email = user.emai
+        this.username = user.username
+        this.displayName = user.displayName
+        this.password = user.password
+        this.agreeToTerms = user.agreeToTerms
+        this.avatar = user.avatar
+        this.bio = user.bio
+        this.banner = user.banner
+        this.bannerColor = user.bannerColor
+        this.dateOfBirth = user.dateOfBirth
+        this.isVerified = user.isVerified
+        this.status = user.status
+        this.lastOnline = user.lastOnline
+        this.createdAt = user.createdAt
+        this.updatedAt = user.updatedAt
+    }
+
+    static from(user) {
+        return new RegisterResponseDto(user)
+    }
+}
