6114c4bc245003aad5d6d0991b72e9789733209d
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-06-02 01:07:03 +0600

    feat: implement async error handling in register controller

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -3,20 +3,17 @@ import { RegisterRequestDto, RegisterResponseDto } from './auth.dto.js'
 import ApiResponse from '../../common/api-response.js'
 import { CREATED } from '../../constants/http-status.js'
 import { USER_CREATED } from '../../constants/app-messages.js'
+import catchAsync from '../../common/catch-async.js'
 
-export const register = async (req, res, next) => {
-    try {
-        const requestDto = RegisterRequestDto.from(req.body)
-        const userData = await authService.registerUser(requestDto)
-        const responseDto = RegisterResponseDto.from(userData)
+export const register = catchAsync(async (req, res) => {
+    const requestDto = RegisterRequestDto.from(req.body)
+    const userData = await authService.registerUser(requestDto)
+    const responseDto = RegisterResponseDto.from(userData)
 
-        ApiResponse.success({
-            res,
-            data: responseDto,
-            statusCode: CREATED,
-            message: USER_CREATED
-        })
-    } catch (err) {
-        next(err)
-    }
-}
+    ApiResponse.success({
+        res,
+        data: responseDto,
+        statusCode: CREATED,
+        message: USER_CREATED
+    })
+})
