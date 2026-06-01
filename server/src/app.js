baab34f74454de60215082bcb7f3e590eba01b93
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-06-02 01:25:19 +0600

    feat: add unknown routes handler middleware for 404 error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,28 @@
 import express from 'express'
-import router from './routes/index.js'
-import { errorHandler } from './middlewares/error-handler.middleware.js'
 import helmet from 'helmet'
 import cors from 'cors'
 import morgan from 'morgan'
 import hpp from 'hpp'
 import logger from './utils/logger.js'
+import router from './routes/index.js'
 import requestIdAttach from './middlewares/request-id.middleware.js'
 import corsConfig from './config/cors.js'
+import { errorHandler } from './middlewares/error-handler.middleware.js'
+import unknownRoutesHandler from './middlewares/unknown-routes.middleware.js'
 
 const createApp = () => {
     const app = express()
 
     app.use(requestIdAttach)
     app.use(helmet())
-    app.use(cors(corsConfig))
     app.use(hpp())
-    app.use(morgan('combined', { stream: logger.stream }))
     app.use(express.json({ limit: '10kb' }))
+    app.use(morgan('combined', { stream: logger.stream }))
+    app.use(cors(corsConfig))
 
     app.use('/api', router)
 
+    app.use(unknownRoutesHandler)
     app.use(errorHandler)
 
     return app
---
+++ server/src/middlewares/unknown-routes.middleware.js
@@ -0,0 +1,8 @@
+import AppError from '../common/app-error.js'
+import { NOT_FOUND } from '../constants/http-status.js'
+
+const unknownRoutesHandler = (req, res, next) => {
+    next(new AppError(`Route: ${req.originalUrl} not found`, NOT_FOUND))
+}
+
+export default unknownRoutesHandler
