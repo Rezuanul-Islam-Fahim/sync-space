00f6023945eee6a68bacb61393255ef1c04b5495
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-06-05 23:52:13 +0600

    feat: update zod dependency to version 4.4.3 and refactor axiosClient to use config for baseURL

--- client/src/shared/api/axiosClient.js
+++ client/src/shared/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import config from '../config/env';
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: config.vite_api_url,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
