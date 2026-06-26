40af3f1deccfedc787952b68ae1415772d5cbf0d
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-06-27 00:24:05 +0600

    Replace (status) boolean with statusCode in api response

--- server/src/common/api-response.js
+++ server/src/common/api-response.js
@@ -1,11 +1,13 @@
+import { DEFAULT_SUCCESS } from '../constants/app-messages.js';
+
 export default class ApiResponse {
-    static success({ res, data, statusCode, message = 'OK' }) {
-        return res.status(statusCode).json({ success: true, data, message });
+    static success({ res, data, statusCode, message = DEFAULT_SUCCESS }) {
+        return res.status(statusCode).json({ statusCode, data, message });
     }
 
     static error({ res, statusCode, message, errors, stack }) {
         return res.status(statusCode).json({
-            success: false,
+            statusCode,
             message,
             errors,
             stack,
