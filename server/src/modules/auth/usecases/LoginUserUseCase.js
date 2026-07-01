d0942e5da227baa8f3bfbacb46ce58e33f69085f
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-07-01 20:01:01 +0600

    Create LoginuserUseCase

---
+++ server/src/modules/auth/usecases/LoginUserUseCase.js
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt';
+import AppError from '../../../common/app-error';
+import {
+    INVALID_CREDENTIALS,
+    UNAUTHORIZED,
+} from '../../../constants/http-status';
+import { generateToken } from '../../../utils/jwt.util';
+
+export class LoginUserUseCase {
+    constructor({ userRepository }) {
+        this.userRepository = userRepository;
+    }
+
+    async execute(data) {
+        const user = await this.userRepository.findByEmail(data.email);
+
+        if (!user) {
+            throw new AppError(INVALID_CREDENTIALS, UNAUTHORIZED);
+        }
+
+        const isMatch = await bcrypt.compare(data.password, user.password);
+
+        if (!isMatch) {
+            throw new AppError(INVALID_CREDENTIALS, UNAUTHORIZED);
+        }
+
+        const tokens = generateToken(user._id, user.email);
+
+        return { user, tokens };
+    }
+}
