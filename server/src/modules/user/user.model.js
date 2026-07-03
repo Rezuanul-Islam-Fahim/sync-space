9ed2c5607305b0d856a442026abed7ae3dd36273
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-07-04 01:23:47 +0600

    Add index in (status, lastOnline) for user model

--- server/src/modules/user/user.model.js
+++ server/src/modules/user/user.model.js
@@ -62,6 +62,8 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+userSchema.index({ status: 1, lastOnline: -1 });
+
 const transform = (doc, ret) => {
     delete ret.password;
     delete ret.__v;
