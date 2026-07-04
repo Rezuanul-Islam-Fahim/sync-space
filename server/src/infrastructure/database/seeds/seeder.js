ff761793f8e12e55f436db70a9c0b4b89b1f9fff
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-07-04 16:05:13 +0600

    Add production environment check to seeder

--- server/src/infrastructure/database/seeds/seeder.js
+++ server/src/infrastructure/database/seeds/seeder.js
@@ -2,9 +2,17 @@ import logger from '../../../utils/logger.js';
 import { closeDB, initDB } from '../connection.js';
 import { User } from '../../../modules/user/index.js';
 import { getSeedUsers } from './user.seed.js';
+import config from '../../../config/index.js';
 
 const runSeeder = async () => {
     try {
+        if (config.env === 'production') {
+            logger.error(
+                'CRITICAL: Seeding aborted! Database seeding is not permitted in production environment.'
+            );
+            process.exit(1);
+        }
+
         logger.info('Starting database seeding...');
 
         await initDB();
