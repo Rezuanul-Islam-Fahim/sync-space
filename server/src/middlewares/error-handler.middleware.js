337eba75ccbfc2234ba6ce282ef070558ad2b422
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-07-05 12:30:53 +0600

    Add requestId to error logging in errorHandler middleware for better traceability

--- server/src/middlewares/error-handler.middleware.js
+++ server/src/middlewares/error-handler.middleware.js
@@ -10,11 +10,13 @@ export const errorHandler = (err, req, res, _next) => {
         ? err.statusCode || INTERNAL_SERVER_ERROR
         : INTERNAL_SERVER_ERROR;
     const message = isOperational ? err.message : DEFAULT_ERROR;
+    const requestId = req.id;
 
     logger.error(err.message, {
         statusCode,
         stack: err.stack,
         isOperational,
+        requestId,
         path: req.originalUrl,
         method: req.method,
         ip: req.ip,
