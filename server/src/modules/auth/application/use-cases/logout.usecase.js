b46d2341eccd5ecb7eb06bb410a33181eae471c5
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-09-03 14:56:22 +0600

    Add LogoutUseCase to handle user logout logic and session clearing

---
+++ server/src/modules/auth/application/use-cases/logout.usecase.js
@@ -0,0 +1,41 @@
+import { UnauthorizedError } from '../../../../shared/error/index.js';
+import { maskEmail } from '../../../../shared/util/index.js';
+import { SESSION_EXPIRED_INVALID } from '../../domain/auth-user.constant.js';
+
+export class LogoutUseCase {
+    constructor({
+        tokenVerifier,
+        refreshTokenReader,
+        refreshTokenWriter,
+        logger,
+    }) {
+        this.tokenVerifier = tokenVerifier;
+        this.refreshTokenReader = refreshTokenReader;
+        this.refreshTokenWriter = refreshTokenWriter;
+        this.logger = logger;
+    }
+
+    async execute(data) {
+        const {
+            sub: authUserId,
+            email,
+            sessionId,
+        } = await this.tokenVerifier.verifyRefreshToken(data.token);
+
+        const refreshToken = await this.refreshTokenReader.get(
+            authUserId,
+            sessionId
+        );
+
+        if (!refreshToken || refreshToken !== data.token) {
+            throw new UnauthorizedError(SESSION_EXPIRED_INVALID);
+        }
+
+        await this.refreshTokenWriter.delete(sessionId, authUserId);
+
+        this.logger.info('Session cleared successful', {
+            authUserId,
+            email: maskEmail(email),
+        });
+    }
+}
